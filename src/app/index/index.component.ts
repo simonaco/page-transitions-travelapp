ecef43bff89ea634adb27218593ca49e65bf8381
Simona Cotin <simona.cotin@gmail.com>
2018-05-19 11:48:38 +0100

    add mapboxgl

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.sass']
+  styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
   places = [];
@@ -11,5 +11,12 @@ export class IndexComponent implements OnInit {
   page;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    mapboxgl.accessToken =
+      'pk.eyJ1Ijoic2RyYXNuZXIiLCJhIjoiY2pmZzBqZmptMjI1eTMzbWl1bGExMHppZyJ9.diPXryPOiyMuqcV4mpNOlg';
+    const map = new mapboxgl.Map({
+      container: 'mapcontain',
+      style: 'mapbox://styles/sdrasner/cjfg0watl6rkv2sllf6hepdd5'
+    });
+  }
 }
