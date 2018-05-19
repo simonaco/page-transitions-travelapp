e460be8e2f7dbe4c64cad5cb989fd65f053ae6a7
Simona Cotin <simona.cotin@gmail.com>
2018-05-19 18:06:43 +0100

    update route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { StoreService } from './store.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public router: Router, private storeService: StoreService) {
+    router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/') {
+          this.storeService.updatePage('index');
+        } else {
+          this.storeService.updatePage(event.url.substring(1));
+        }
+      }
+    });
+  }
 }
