c277fcb268c5f29b277c2c43a2d7b5ff675f6e2a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-05-22 20:40:42 +0200

    Finish port of rating component
    - inputs and implementation details
    - fixes for star colors
    - markup cleanup
    
    Thanks!

--- src/app/icons/icon-star-full.component.ts
+++ src/app/icons/icon-star-full.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: '[icon-star-full]',
   template: `
   <svg:path d="M12 17.25l-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609 7.172 0.609-5.438 4.734 1.641 7.031z"/>
-  `,
-  styles: []
+  `
 })
-export class StarFullComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class IconStarFullComponent { }
