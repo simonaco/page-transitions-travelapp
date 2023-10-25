f2ed8df32dc1e9f6ef905af8ac1c2e121545af19
Simona Cotin <simona.cotin@gmail.com>
2023-10-25 14:45:51 +0200

    update to v16

--- server.ts
+++ server.ts
@@ -1,51 +1,74 @@
-import 'zone.js/node';
-import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
-// Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import "zone.js/node";
 
-import * as express from 'express';
-import {join} from 'path';
+import { APP_BASE_HREF } from "@angular/common";
+import { ngExpressEngine } from "@nguniversal/express-engine";
+import * as express from "express";
+import { existsSync } from "fs";
+import { join } from "path";
 
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+import { AppServerModule } from "./src/main.server";
 
-// Express server
-const app = express();
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+  const distFolder = join(process.cwd(), "dist/browser");
+  const indexHtml = existsSync(join(distFolder, "index.original.html"))
+    ? "index.original.html"
+    : "index";
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
+  server.engine(
+    "html",
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+  server.set("view engine", "html");
+  server.set("views", distFolder);
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+  // TODO: implement data requests securely
+  server.get("/api/**", (req, res) => {
+    res.status(404).send("data requests are not yet supported");
+  });
 
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
+  // Serve static files from /browser
+  server.get(
+    "*.*",
+    express.static(distFolder, {
+      maxAge: "1y",
+    })
+  );
 
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
+  // All regular routes use the Universal engine
+  server.get("*", (req, res) => {
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
+  });
 
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-  maxAge: '1y'
-}));
+  return server;
+}
 
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
+function run(): void {
+  const port = process.env["PORT"] || 4000;
 
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || "";
+if (moduleFilename === __filename || moduleFilename.includes("iisnode")) {
+  run();
+}
+
+export * from "./src/main.server";
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,43 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   template: `
-  <footer>
-  <div>
-  Original project by
-  <a
-    href="https://twitter.com/sarah_edo"
-    target="_blank"
-    rel="noopener">
-    sarah_edo
-  </a>, built with
-  <a
-    href="https://github.com/sdras/page-transitions-travelapp"
-    target="_blank"
-    rel="noopener"
-  >
-    Vue & Nuxt (code)
-  </a>
-</div>
-<div>
-  You are viewing a fork adapted by
-  <a
-    href="https://twitter.com/simona_cotin"
-    target="_blank"
-    rel="noopener">
-  simona_cotin
-</a>, built with
-<a
-  href="https://github.com/simonaco/page-transitions-travelapp"
-  target="_blank"
-  rel="noopener"
->
-  Angular & RxJS (code)
-</a>
-</div>  </footer>
+    <footer>
+      <div>
+        Original project by
+        <a href="https://twitter.com/sarah_edo" target="_blank" rel="noopener">
+          sarah_edo </a
+        >, built with
+        <a
+          href="https://github.com/sdras/page-transitions-travelapp"
+          target="_blank"
+          rel="noopener"
+        >
+          Vue & Nuxt (code)
+        </a>
+      </div>
+      <div>
+        You are viewing a fork adapted by
+        <a
+          href="https://twitter.com/simona_cotin"
+          target="_blank"
+          rel="noopener"
+        >
+          simona_cotin </a
+        >, built with
+        <a
+          href="https://github.com/simonaco/page-transitions-travelapp"
+          target="_blank"
+          rel="noopener"
+        >
+          Angular (code)
+        </a>
+      </div>
+    </footer>
   `,
-  styleUrls: ['./footer.component.css']
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
   constructor() {}
