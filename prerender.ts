3c1bbea13dd29ab79f39afbadca843a8d87d7c77
Simona Cotin <simona.cotin@gmail.com>
2023-10-25 12:16:27 +0200

    update 11 -> 12

--- prerender.ts
+++ prerender.ts
@@ -1,5 +1,5 @@
 // Load zone.js for the server.
-import 'zone.js/dist/zone-node';
+import 'zone.js/node';
 import 'reflect-metadata';
 import {readFileSync, writeFileSync, existsSync, mkdirSync} from 'fs';
 import {join} from 'path';
